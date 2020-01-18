9f74fb294ffd4eaad2625797f0ef408a692a51bc
Adam <adam.lee346@gmail.com>
2020-01-18 13:28:30 -0700

    print request example

--- wordcount/views.py
+++ wordcount/views.py
@@ -5,7 +5,8 @@ def home(request):
     return render(request, 'home.html', {'message':'Welcome to the home page'})
 
 def count(request):
+    fulltext = request.GET['fulltext']
     return render(request, 'count.html')
-
+    print(fulltext)
 def about(request):
     return render(request, 'about.html')
